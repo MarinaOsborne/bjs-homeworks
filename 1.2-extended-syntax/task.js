6015c18f83542dad1a67edd14d43cb9ade02e14b
Marina Osborne <marina08088@mail.ru>
2020-08-20 22:09:27 +0300

    Внесены правки

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,11 +2,9 @@
 function getResult(a, b, c) { 
   let d = b **2 - 4 * a * c;
   let x = [];
-  if (d < 0) {
-     x = [];
-  } else if (d === 0) {
+  if (d === 0) {
     x = [-b / (2 * a)];
-  } else {
+  } else if (d > 0) {
     x = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
      }
   return x;
@@ -18,31 +16,26 @@ function getResult(a, b, c) {
 
 let marks = [] 
 function getAverageMark(marks) {
-  let sum = 0;
-    for (let i = 0; i < marks.length; i++) {
-      sum += marks[i];
+  if (marks.length === 0) {
+    return 0;
+  } else {
+     let newArr = marks.slice(0, 5);     
+     let sum = 0;
+     for (let i = 0; i < newArr.length; i++) {
+      sum += newArr[i]
+     }
+     let result = Math.floor(sum / newArr.length);
+     return result;
     }
-  let average = Math.floor(sum / marks.length);
-
- if (marks.length === 0) {
-   return 0;
-  } else if (marks.length > 5) {
-      marks.slice(0, 5);
-      alert('Учитываются только первые 5 оценок! Средний балл: ' + average)
-    } else {   
-      alert('Средний балл: ' + average)
-      }
-   
 }
 
 function askDrink(name,dateOfBirthday){
   let yearOfBirthday = dateOfBirthday.getFullYear();
   let date = new Date();
-  let currentYear = date.getFullYear();
-  if ((currentYear - yearOfBirthday) > 18 ) {
-    alert('Не желаете ли олд-фэшн, ' + name + '?');
+  if ((date.getFullYear() - yearOfBirthday) > 18 ) {
+    return 'Не желаете ли олд-фэшн, ' + name + '?';
   } else {
-    alert('Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!');
+    return 'Сожалею, ' + name + ', но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!';
   }
     // return result;
 }
