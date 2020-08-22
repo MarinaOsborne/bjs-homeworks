b004b702bfa1769ee7b95b92d9242e19778a00e4
Marina Osborne <marina08088@mail.ru>
2020-08-22 22:12:46 +0300

    ДЗ 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict'
+let percent = 0;
+let contribution = 0;
+let amount = 0;
+let date = 0;
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+   if (isNaN(percent) === true) {
+    return 'Параметр Процентная ставка содержит неправильное значение'
+    } else if (isNaN(contribution) === true) {
+        return 'Параметр Начальный взнос содержит неправильное значение'
+    } else if (isNaN(amount) === true) {
+        return 'Параметр Общая стоимость содержит неправильное значение'
+    } else {
+   let mortgageBody = (parseFloat(amount) - parseFloat(contribution));
+   let monthLeft = Math.floor((new Date("08/22/2021") - new Date())/ (1000 * 3600 * 24)/29);
+   let p = parseFloat(percent) / 1200;
+   let monthlyPayment = mortgageBody * (p + p /(Math.pow((1 + p), monthLeft) - 1));
+   let totalAmount = monthLeft * monthlyPayment;
+   return +totalAmount.toFixed(2);
+   }
+   
 }
 
 function getGreeting(name) {
+    if (name === "" || name === null || name === undefined) {
+        return 'Привет, мир! Меня зовут Аноним.'
+        } else {
+            return 'Привет, мир! Меня зовут ' + name + '.'
+        }
     // код для задачи №2 писать здесь
     // return greeting;
 }
