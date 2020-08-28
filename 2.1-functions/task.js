4e2c060e78526960d0b327f0fe089148be0149a8
Marina Osborne <marina08088@mail.ru>
2020-08-28 13:04:06 +0300

    Правки 2808

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -13,15 +13,16 @@ function getSolutions( a, b, c ) {
         return result = {D: D, roots: [x1, x2]};
     }
   }
-  getSolutions( a, b, c );
-function showSolutionsMessage( a, b, c ) {
+  
+  function showSolutionsMessage( a, b, c ) {
+  let result = getSolutions( a, b, c);
    console.log('Вычисляем корни квадратного уравнения ' + a + 'x²' + ' + ' + b + 'x' + ' + ' + c + '\n' +'Значение дискриминанта: ' + result.D);
   if  (result.D < 0) {
         console.log('Уравнение не имеет вещественных корней');
     } else if (result.D === 0) {
         console.log ('Уравнение имеет один корень X₁ = ' + result.roots);
     } else {
-        console.log ('Уравнение имеет два корня X₁ = ' + result.roots[0] + 'X₂ = ' + result.roots[1]);
+        console.log ('Уравнение имеет два корня X₁ = ' + result.roots[0] + ', X₂ = ' + result.roots[1]);
     }
 }
 
@@ -50,6 +51,7 @@ function getAverageScore(data) {
   }
   data.average = (getAverageMark(Object.values(data)));
   }
+  return data;
 }
 
    let marks = [2, 4, 5];
