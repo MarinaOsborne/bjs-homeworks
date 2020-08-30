f026fb2c2e3b88e6888918003831be3bbdbfbd97
Marina Osborne <marina08088@mail.ru>
2020-08-30 14:13:01 +0300

    ДЗ 2,2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+'use strict'
+let str = 'anna';
+String.prototype.isPalindrome = function(str) {
+  str = this.toLowerCase().replace(/\s/g,'');
+   return str === str.split('').reverse().join(''); 
 }
 
+
+let marks = [];
+
+function getAverageMark(marks) {
+  if (marks.length === 0) {
+    return 0;
+  } else {
+  let sum = 0; 
+    for (let i = 0; i < marks.length; i++) {
+      sum += marks[i];  
+    }  
+      let average = sum / marks.length; 
+      let roundedAverage = Math.round(average);
+      return roundedAverage;
+    }
+    
+  }
+    
+    
+
+
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    
+    let date = new Date(birthday);
+    let now = Date.now();
+    birthday = +date;
+    let diff = now - birthday;
+    let age = diff / 31536000000;
+    let verdict = (age > 18) ? true : false; 
+    return verdict;   
 }
